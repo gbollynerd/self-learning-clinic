441e3e60ae2088059b54326053e8d2eafb94fb69
Ajayi Omogbolade <aomogbolade@gmail.com>
2017-04-08 15:22:44 +0100

    Edited Fizzbuzz script

--- Fizzbuzz/app/app.js
+++ Fizzbuzz/app/app.js
@@ -2,7 +2,7 @@
 /**
 a function called *fizzBuzz* to return 'Fizz', 
 'Buzz', 'FizzBuzz', or the argument it receives, 
-all depending on the argument of the function, a number that is divisible by, 3, 5, or both 3 and 5, respectively.
+all depending on the argument of the function, a number that is divisible by, 3, 5, or both 3 and 5, respectively..
 **/
 
 module.exports = {

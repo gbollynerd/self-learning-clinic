200fbf3d0a4d1ea2bebf881ca38ae11595b944aa
gbollynerd <aomogbolade@gmail.com>
2017-04-10 11:18:14 +0100

    Creating my fizzbuzz function in app.js

--- Fizzbuzz/app/app.js
+++ Fizzbuzz/app/app.js
@@ -7,23 +7,6 @@ all depending on the argument of the function, a number that is divisible by, 3,
 
 module.exports = {
 fizzBuzz : function(number){
-    var result;   
-    if((number % 3 === 0) && (number % 5 === 0)) {
-       //return FizzBuzz for number divisible by 3 and 5
-      result = "FizzBuzz";
-    }
-    else if(number % 3 === 0){
-      //return Fizz for number divisible by 3
-      result = "Fizz";
-    }
-    else if(number % 5 === 0){
-      //return Buzz for number divisible by 5
-      result = "Buzz";
-    }
-    else {
-      //return the number for number not divisible by 3, 5 or 3 and 5
-      result = number;
-    }
-    return result;
+    
   }
  }

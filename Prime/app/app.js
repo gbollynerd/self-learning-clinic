5d481214b5a93eea89952fe8afd3fcb599aee03a
gbollynerd <aomogbolade@gmail.com>
2017-04-10 12:01:11 +0100

    Updating app.js file

--- Prime/app/app.js
+++ Prime/app/app.js
@@ -5,6 +5,35 @@ module.exports = {
  /* Find the average of three integers */
  {
 
- 
+ /* Find the average of three integers */
+ isPrimes : function(num){
+  if (typeof num === 'number'){
+    var isPrime = false;  
+      if (num  <= 1){
+        return isPrime;
+      }
+  
+      for (var i = 2; i < num; i++) {     
+        if (num % i === 0 ) {     
+          return !isPrime;    
+      }
+    }
+    return isPrime; 
+  }   
+},
+
+getPrimes : function(num){
+  if (typeof num === 'number'){
+    var primeList = [];
+    for (var i = 2; i < num; i++) {
+      if (!this.isPrimes(i)) {
+        primeList.push(i);
+      }
+    }
+    return primeList;
+  }else
+    return [];    
+  }  
+}
 
 }

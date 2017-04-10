b19007fd33e6f156763e18143b73623110d05587
Ajayi Omogbolade <aomogbolade@gmail.com>
2017-04-10 19:54:23 +0100

    Adding the OOP Javascript script

---
+++ OOP/OOP.js
@@ -0,0 +1,10 @@
+/*
+	This script shows how to implement function object as a class in a real world senario
+	of a Boss and the staffs
+*/
+
+//First define an empty function
+
+let BossDetails = function() { };
+
+

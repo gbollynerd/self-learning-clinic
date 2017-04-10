dc51c80a3a8595547485aa1c89b8baf0310c0f65
Ajayi Omogbolade <aomogbolade@gmail.com>
2017-04-10 19:56:12 +0100

    Creating Boss Detail properties to the function

--- OOP/OOP.js
+++ OOP/OOP.js
@@ -7,4 +7,14 @@
 
 let BossDetails = function() { };
 
+//use the prototype keyword to set properties of the function object
+
+//BossDetail first property
+
+BossDetails.prototype.bossDetail = {
+  bossName : "Ajayi Omogbolade",
+  sections : ["Human Resource", "Finance", "Customer Care", "IT", "Sales", "Marketing"],
+  bossAddress : "12, Samuel Onafuwa, Rivervalley"
+  
+};
 

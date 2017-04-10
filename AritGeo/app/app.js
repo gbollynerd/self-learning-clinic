c1eff2e1dcd7ba6834f315b16250f9f13a25326d
gbollynerd <aomogbolade@gmail.com>
2017-04-10 11:37:31 +0100

    Creating AritGeo Folder

---
+++ AritGeo/app/app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+module.exports = {
+
+ /* Find the average of three integers */
+	aritGeo : function(){
+  		if (arguments.length === 0){
+    		return 0;
+  		}
+
+  		if (arguments.length <= 2){
+    		return -1;
+  		}
+    
+    
+	    
+  	}
+	  return -1;
+	}
+}
